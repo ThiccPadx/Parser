f887b9c7e7dc984ae5d5345dc461c10e4f664fca
@raywom <40425058+raywom@users.noreply.github.com>
2023-07-26 22:25:37 +0200

    refactoring

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Parser
 {
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("");
+        static ITelegramBotClient bot = new TelegramBotClient("5731506005:AAFtaa1BzbLG2M_AuQqEWWFBTOnL8X2WG-g");
 
         private static async Task Main(string[] args)
         {
@@ -36,26 +36,30 @@ namespace Parser
             bool isAlmatyChecked = false;
             while (true)
             {
-                while (isAstanaChecked == false)
+                while (!isAstanaChecked)
                 {
                     var genCaptcha = GenerateCaptcha();
-                    var convertImage = Base64ToImage(genCaptcha.Result.imageInBase64);
+                    if (genCaptcha.Result != null)
                     {
-                        OCRResult = engine.DetectText(convertImage);
+                        var convertImage = Base64ToImage(genCaptcha.Result.imageInBase64);
+                        {
+                            OCRResult = engine.DetectText(convertImage);
+                        }
                     }
+
                     CaptchaVerifier? token = new CaptchaVerifier();
 
-                    if (OCRResult != null)
-                        token = await VerifyCaptcha(OCRResult.Text, genCaptcha.Result.id);
+                    if (OCRResult != null && genCaptcha.Result != null) token = await VerifyCaptcha(OCRResult.Text, genCaptcha.Result.id);
 
                     if (token?.token != null)
                     {
                         var checkBook = CheckBookingAstana(token.token);
-                        if (checkBook.Result.DaysTable.Length > 0)
+                        if (checkBook.Result is { DaysTable.Length: > 0 })
                         {
-                            WriteToAllUsers("Запись появилась в Астане");
-                            Console.WriteLine("Booking is available in Astana");
-                            //RefrenSolo();
+                            //parse array of strings to string
+                            var days = string.Join(", ", checkBook.Result.DaysTable);
+                            await WriteToAllUsersAstana("Запись есть в Астане на " + days);
+                            Console.WriteLine("Booking is available in Astana on " + days);
                         }
                         else
                         {
@@ -71,7 +75,7 @@ namespace Parser
                     }
                 }
 
-                while (isAlmatyChecked == false)
+                while (!isAlmatyChecked)
                 {
                     var genCaptcha = GenerateCaptcha();
                     var convertImage = Base64ToImage(genCaptcha.Result.imageInBase64);
@@ -88,8 +92,9 @@ namespace Parser
                         var checkBook = CheckBookingAlmaty(token.token);
                         if (checkBook.Result.DaysTable.Length > 0)
                         {
-                            WriteToAllUsers("Запись появилась в Алмате");
-                            Console.WriteLine("Booking is available in Almaty");
+                            var days = string.Join(", ", checkBook.Result.DaysTable);
+                            await WriteToAllUsers("Запись есть в Алмате на " + days);
+                            Console.WriteLine("Booking is available in Almaty on " + days);
                         }
                         else
                         {
@@ -104,16 +109,16 @@ namespace Parser
                     }
                 }
 
-                int milliseconds = 300000;
+                int milliseconds = 1800000;
                 Thread.Sleep(milliseconds);
                 isAstanaChecked = false;
                 isAlmatyChecked = false;
             }
         }
 
-        public static async void WriteToAllUsers(string message)
+        public static async Task WriteToAllUsers(string message, string city)
         {
-            var users = await GetUsers();
+            var users = await GetUsers(city);
             foreach (var user in users)
             {
                 try
@@ -143,14 +148,12 @@ namespace Parser
 
         public static async Task<bool> IsUserExist(string id)
         {
-            using (StreamReader reader = new StreamReader("users.txt"))
+            using StreamReader reader = new StreamReader("users.txt");
+            string? line;
+            while ((line = await reader.ReadLineAsync()) != null)
             {
-                string? line;
-                while ((line = await reader.ReadLineAsync()) != null)
-                {
-                    if (id == line)
-                        return true;
-                }
+                if (id == line)
+                    return true;
             }
 
             return false;
@@ -158,19 +161,16 @@ namespace Parser
 
         public static async void AddUser(string id)
         {
-            return;
+            await File.AppendAllTextAsync("users.txt", id + Environment.NewLine);
         }
 
-        public static async Task<List<long>> GetUsers()
+        public static async Task<List<long>> GetUsers(string city)
         {
-            List<long> users = new List<long>();
-            using (StreamReader reader = new StreamReader("users.txt"))
+            var users = new List<long>();
+            using StreamReader reader = new StreamReader("users" + city + ".txt");
+            while (await reader.ReadLineAsync() is { } line)
             {
-                string? line;
-                while ((line = await reader.ReadLineAsync()) != null)
-                {
-                    users.Add(long.Parse(line));
-                }
+                users.Add(long.Parse(line));
             }
 
             return users;
@@ -179,8 +179,6 @@ namespace Parser
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
             CancellationToken cancellationToken)
         {
-            // Некоторые действия
-            //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
             try
             {
                 if (!IsUserExist(update.Message.From.Id.ToString()).Result)
@@ -192,6 +190,7 @@ namespace Parser
 
                 if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
                 {
+                    Console.WriteLine(update.Message.Text);
                     var message = update.Message;
                     if (message.Text.ToLower() == "/start")
                     {
@@ -212,13 +211,20 @@ namespace Parser
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception,
             CancellationToken cancellationToken)
         {
-            // Некоторые действия
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
         }
 
-        public static Task<CheckBooking?> CheckBookingAlmaty(string token)
+        public static Task<CheckBooking?> CheckBooking(string token, string city)
         {
-            var url = "https://api.e-konsulat.gov.pl/api/rezerwacja-wizyt-wizowych/terminy/410";
+            string url;
+            if (city == "Astana")
+            {
+                url = "https://api.e-konsulat.gov.pl/api/rezerwacja-wizyt-wizowych/terminy/1351";
+            }
+            else
+            {
+                url = "https://api.e-konsulat.gov.pl/api/rezerwacja-wizyt-wizowych/terminy/410";
+            }
 
             var request = WebRequest.Create(url);
             request.Method = "POST";
@@ -238,39 +244,6 @@ namespace Parser
             reqStream.Write(byteArray, 0, byteArray.Length);
 
             using var response = request.GetResponse();
-            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-
-            using var respStream = response.GetResponseStream();
-
-            using var reader = new StreamReader(respStream);
-            string data = reader.ReadToEnd();
-            var checkBook = JsonSerializer.Deserialize<CheckBooking>(data);
-            return Task.FromResult(checkBook);
-        }
-
-        public static Task<CheckBooking?> CheckBookingAstana(string token)
-        {
-            var url = "https://api.e-konsulat.gov.pl/api/rezerwacja-wizyt-wizowych/terminy/1351";
-
-            var request = WebRequest.Create(url);
-            request.Method = "POST";
-
-            var values = new Dictionary<string, string>
-            {
-                {"token", token}
-            };
-
-            var json = JsonSerializer.Serialize(values);
-            byte[] byteArray = Encoding.UTF8.GetBytes(json);
-
-            request.ContentType = "application/json";
-            request.ContentLength = byteArray.Length;
-
-            using var reqStream = request.GetRequestStream();
-            reqStream.Write(byteArray, 0, byteArray.Length);
-
-            using var response = request.GetResponse();
-            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
 
             using var respStream = response.GetResponseStream();
 
@@ -337,23 +310,18 @@ namespace Parser
             reqStream.Write(byteArray, 0, byteArray.Length);
 
             using var response = request.GetResponse();
-            // Console.WriteLine(((HttpWebResponse)response).StatusDescription);
 
             using var respStream = response.GetResponseStream();
 
             using var reader = new StreamReader(respStream);
             string data = reader.ReadToEnd();
             var captcha = JsonSerializer.Deserialize<CaptchaGenerator>(data);
-            // Console.WriteLine(data);
-            // data = JsonSerializer.Deserialize<string>(data);
             return Task.FromResult(captcha);
         }
 
         public static Image Base64ToImage(string base64String)
         {
-            // Convert base 64 string to byte[]
             byte[] imageBytes = Convert.FromBase64String(base64String);
-            // Convert byte[] to Image
             using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
             {
                 Image image = Image.FromStream(ms, true);
